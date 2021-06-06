7619502ef467871b3487905f135bce451ee46898
josubin47 <3_3_jimca@naver.com>
2021-06-06 20:40:15 +0900

    feat : 요구사항 2 - 회원가입

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,10 +3,15 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
 
+import db.DataBase;
+import model.User;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -24,26 +29,45 @@ public class RequestHandler extends Thread {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
 
-            //요구사항 1 - index.html 응답하기
             BufferedReader buffer = new BufferedReader(new InputStreamReader(in, "UTF-8"));
 
             String line = buffer.readLine();
-
-            String[] token = line.split(" ");
-            String url = token[1]; // request url
-
-            log.debug("[url]" + url);
-
-            int count = 0;
 
             if(StringUtils.equals(line, null)) {
                 return;
             }
 
-            //헤더 끝까지
+            String[] token = line.split(" ");
+            String url = token[1]; // request urL
+
+            Map<String, String> params = new HashMap<>();
+
+            int index = url.indexOf("?");
+
+            if(StringUtils.equals(url, "/")) {
+                url += "index.html";
+            } else if(index != -1) {
+                //전달된 정보가 있으면
+                params = HttpRequestUtils.parseQueryString(url.substring(index+1));
+                url = url.substring(0, index);
+            }
+
+            log.debug("[url]" + url);
+            log.debug("[params]" + params);
+
+            if(StringUtils.equals(url, "/user/create")) {
+                //회원가입 정보 저장
+                User userInfo = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+                DataBase.addUser(userInfo);
+            }
+
+            //아 하기싫어
+            //HTTP 정보 읽어오기
             while (!StringUtils.equals(line, "")) {
                 line = buffer.readLine();
-                log.debug("[test]" + line);
+                log.debug("[line info]" + line);
+
+
 
             }
 
@@ -58,6 +82,7 @@ public class RequestHandler extends Thread {
             log.error(e.getMessage());
         }
     }
+
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
