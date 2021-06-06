635797e73a8df78efa978ee5776eba33b67f7d0c
josubin47 <3_3_jimca@naver.com>
2021-06-07 00:15:12 +0900

    feat : 요구사항 6 - 사용자 목록 출력

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,6 +3,7 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,9 +19,12 @@ import util.IOUtils;
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
-    private static final String SIGN_UP_URL = "/user/create";
-    private static final String LOGIN_URL = "/user/login";
+    private static final String SIGN_UP_PATH = "/user/create";
+    private static final String LOGIN_PATH = "/user/login";
+    private static final String USER_LIST_PATH = "/user/list";
+    private static final String LOGIN_URL = "/user/login.html";
     private static final String LOGIN_FAILED_URL = "/user/login_failed.html";
+
 
     private Socket connection;
 
@@ -38,7 +42,7 @@ public class RequestHandler extends Thread {
 
             String line = buffer.readLine();
 
-            if(StringUtils.equals(line, null)) {
+            if (StringUtils.equals(line, null)) {
                 return;
             }
 
@@ -49,12 +53,12 @@ public class RequestHandler extends Thread {
 
             int index = url.indexOf("?");
 
-            if(StringUtils.equals(url, "/")) {
+            if (StringUtils.equals(url, "/")) {
                 url += "index.html";
-            } else if(index != -1) {
+            } else if (index != -1) {
                 //GET 방식
                 //전달된 정보가 있으면
-                params = HttpRequestUtils.parseQueryString(url.substring(index+1));
+                params = HttpRequestUtils.parseQueryString(url.substring(index + 1));
                 url = url.substring(0, index);
 
                 log.debug("[url]" + url);
@@ -63,6 +67,7 @@ public class RequestHandler extends Thread {
 
 
             int contentLength = 0;
+            boolean isLogin = false;
 
             //HTTP 정보 읽어오기
             while (!StringUtils.equals(line, "")) {
@@ -72,35 +77,69 @@ public class RequestHandler extends Thread {
                 if (line.contains("Content-Length")) {
                     contentLength = Integer.parseInt(line.split(":")[1].trim());
                 }
+
+                if (line.contains("Cookie")) {
+                    isLogin = isLogin(line);
+                }
             }
 
+            switch (url) {
+                case SIGN_UP_PATH: {
+                    //회원가입 정보 저장
+                    String body = IOUtils.readData(buffer, contentLength);
+                    params = HttpRequestUtils.parseQueryString(body);
+                    User userInfo = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+                    DataBase.addUser(userInfo);
 
-            if(StringUtils.equals(url, SIGN_UP_URL)) {
-                //회원가입 정보 저장
-                String body = IOUtils.readData(buffer, contentLength);
-                params = HttpRequestUtils.parseQueryString(body);
-                User userInfo = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
-                DataBase.addUser(userInfo);
+                    DataOutputStream dos = new DataOutputStream(out);
 
-                DataOutputStream dos = new DataOutputStream(out);
-
-                response302Header(dos);
-            } else if (StringUtils.equals(url, LOGIN_URL)) {
-                String body = IOUtils.readData(buffer, contentLength);
-                params = HttpRequestUtils.parseQueryString(body);
-
-                String userId = params.get("userId");
-                String password = params.get("password");
-
-                if(DataBase.checkUserInfo(userId, password)){
-                    responseLoginSuccessHeader(out);
-                } else {
-                    url = LOGIN_FAILED_URL;
-                    response200Header(out, url);
+                    response302Header(dos);
+                    break;
                 }
+                case LOGIN_PATH: {
+                    String body = IOUtils.readData(buffer, contentLength);
+                    params = HttpRequestUtils.parseQueryString(body);
 
-            } else {
-                response200Header(out, url);
+                    String userId = params.get("userId");
+                    String password = params.get("password");
+
+                    if (DataBase.checkUserInfo(userId, password)) {
+                        responseLoginSuccessHeader(out);
+                    } else {
+                        url = LOGIN_FAILED_URL;
+                        response200Header(out, url);
+                    }
+                    break;
+                }
+                case USER_LIST_PATH: {
+                    if (isLogin) {
+                        //로그인 된 상태
+                        Collection<User> userList = DataBase.findAll();
+                        StringBuilder stringBuilder = new StringBuilder();
+
+                        userList.forEach(user ->
+                                {
+                                    stringBuilder.append("<p> 사용자 아이디 : ").append(user.getUserId()).append("</p>");
+                                    stringBuilder.append("<p> 사용자 이름 : ").append(user.getName()).append("</p><br/><br/>");
+                                }
+                        );
+
+                        DataOutputStream dos = new DataOutputStream(out);
+
+                        byte[] body = stringBuilder.toString().getBytes();
+
+                        response200Header(dos, body.length);
+                        responseBody(dos, body);
+
+                    } else {
+                        url = LOGIN_URL;
+                        response200Header(out, url);
+                    }
+                    break;
+                }
+                default:
+                    response200Header(out, url);
+                    break;
             }
 
             in.close();
@@ -108,6 +147,12 @@ public class RequestHandler extends Thread {
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private boolean isLogin(String line) {
+        Map<String, String> result = HttpRequestUtils.parseCookies(line);
+        return StringUtils.equals(result.get("Cookie: logined"), "true");
+
     }
 
     private void response200Header(OutputStream out, String url) throws IOException {
